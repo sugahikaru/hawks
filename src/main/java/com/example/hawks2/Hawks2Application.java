eb27ca48b109f1f83d7b315993321ff403a1b53b
sugahikaru <sh09054713265@outlook.jp>
2024-05-25 15:53:40 +0900

    セットアップ

---
+++ src/main/java/com/example/hawks2/Hawks2Application.java
@@ -0,0 +1,13 @@
+package com.example.hawks2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hawks2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hawks2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hawks2/Hawks2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hawks2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hawks2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
