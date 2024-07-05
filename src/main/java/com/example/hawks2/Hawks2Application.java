43b0e85b0679573a8f46614c94c9560617afc366
sugahikaru <sh09054713265@outlook.jp>
2024-07-05 21:41:32 +0900

    j

--- src/main/java/com/example/hawks2/Hawks2Application.java
+++ src/main/java/com/example/hawks2/Hawks2Application.java
@@ -1,8 +1,5 @@
 package com.example.hawks2;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class Hawks2Application {
 
